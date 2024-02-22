db0c8179b081f1f7427774f7a7b51e528d897222
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-22 19:39:20 +0530

    chat logic done

--- client/src/components/chatPage/ChatPage.jsx
+++ client/src/components/chatPage/ChatPage.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { user } from "../joinPage/JoinPage";
 import { io } from "socket.io-client";
 import send from "../../assets/send.png";
 
+let socket;
+
 const ChatPage = () => {
-  const socket = io("http://localhost:8000/", { transports: ["websocket"] });
+  let [id, setId] = useState("");
+
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
 
   useEffect(() => {
+    socket = io("http://localhost:8000/", { transports: ["websocket"] });
+
     socket.on("connect", () => {
-      // alert("Connected");
+      alert("Connected");
+      setId(socket.id);
     });
 
     socket.emit("joined", { user });
@@ -16,6 +28,7 @@ const ChatPage = () => {
     socket.on("welcome", (data) => {
       console.log(data.user, data.message);
     });
+
     socket.on("userJoined", (data) => {
       console.log(data.user, data.message);
     });
@@ -30,6 +43,14 @@ const ChatPage = () => {
     };
   }, []);
 
+  useEffect(() => {
+    socket.on('sendMessage', (data) => {
+      console.log(data.user, data.message, data.id);
+    });
+
+    return () => {};
+  }, []);
+
   return (
     <div className="chatPage">
       <div className="chatContainer">
@@ -37,7 +58,7 @@ const ChatPage = () => {
         <div className="chatBox"></div>
         <div className="inputBox">
           <input type="text" id="chatInput" />
-          <button className="sendBtn">
+          <button onClick={send} className="sendBtn">
             <img src={send} />
           </button>
         </div>
--- server/app.js
+++ server/app.js
@@ -22,14 +22,18 @@ io.on("connection", (socket) => {
 
   socket.on("joined", ({ user }) => {
     users[socket.id] = user;
-    socket.broadcast.emit("userJoined", {
-      user: "Admin",
-      message: `${users[socket.id]} has joined`,
-    });
     socket.emit("welcome", {
       user: "Admin",
       message: `Welcome to the chat ${users[socket.id]}`,
     });
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+  });
+
+  socket.on("message", ({message, id}) => {
+    io.emit("sendMessage", { user: users[id], message, id });
   });
 
   socket.on("disconnect", () => {
